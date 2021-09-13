8a60ff979e0d74c130c50d25240c8d68e66cd540
Sofia <sofiadurkan@gmail.com>
2021-09-12 23:30:20 -0500

    completed

---
+++ src/utils/customToastify.js
@@ -0,0 +1,13 @@
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const succesNotify = (msg) =>
+  toast.success(msg, {
+    position: 'top-right',
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
